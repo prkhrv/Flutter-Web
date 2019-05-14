f713104474a351d60616e0ce3d42dbbaa00f7a7c
Prakhar Varshney <prakharnegative@gmail.com>
2019-05-15 03:15:15 +0530

    Added Assets and Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -17,46 +17,35 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  final String title;
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  final String title;
+  _MyHomePageState createState() => _MyHomePageState();
+}
 
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (choose the "Toggle Debug Paint" action
-          // from the Flutter Inspector in Android Studio, or the "Toggle Debug
-          // Paint" command in Visual Studio Code) to see the wireframe for each
-          // widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
+    return new Scaffold(
+      body: new Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: new BoxDecoration(
+          image: new DecorationImage(
+            image: new AssetImage('images/bg.png'),
+            fit: BoxFit.cover
+          )
+        ),
+        child: new Column(
           children: <Widget>[
-            Text(
-              'Prakhar',
-            ),
+            new Text(
+                      "Travelkit",
+                      style: TextStyle(fontFamily: 'Pacifico',fontSize: 50.0,color: Colors.white),
+                      ),
           ],
         ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
